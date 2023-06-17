a58ab21071e24501bf91e2d292b726ce9c1b41b3
edna-dande <dandeedna@gmail.com>
2023-06-18 01:04:09 +0300

    add event dispatcher

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
+window.Event = new class {
+    constructor() {
+        this.vue = new Vue();
+    }
+
+    fire(event, data = null) {
+        this.vue.$emit(event,data);
+    }
+
+    listen(event,callback) {
+        this.vue.$on(event, callback);
+    }
+}
+ 
+
 Vue.component('coupon', {
      
     template:`<input placeholder="Enter your coupon code" @blur="onCouponApplied">`,
     
     methods:{
         onCouponApplied() {
-            this.$emit('applied');
+            Event.fire('applied');
         
         }
        }
-
 
 });
 
@@ -21,11 +35,10 @@ new Vue({
         couponApplied: false
     },
 
-    methods:{
-        onCouponApplied() {
-            this.couponApplied = true;
+    created(){
+        Event.listen('applied', () => alert('Handling it!'))
 
-        }
+
        }
     
 });
