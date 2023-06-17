5f5a1b7cc6ab388aa03fd3e29a149a3e1d594f92
edna-dande <dandeedna@gmail.com>
2023-06-17 23:25:38 +0300

    add component-modal

--- main.js
+++ main.js
@@ -1,32 +1,31 @@
-Vue.component('message', {
+Vue.component('modal', {
+     
+    template:`
+     <div class="modal is-active">
+        <div class="modal-background"></div>
+            <div class="modal-content">
+                <div class="box">
 
-    props: ['title', 'body'],
+                <slot></slot> 
 
+                </div>
 
-    data() {
-
-     return {isVisible: true
-
-        };
-    },
-
-    template: `
-    <article class="message" v-show="isVisible">
-        <div class="message-header">
-            {{ title }}
-
-            <button type="button" @click="isVisible = false">x</button>
+            </div>
+        
+            <button class="modal-close" @click="$emit('close')"></button>
         </div>
 
-        <div class="message-body">
-        {{ body }}
-        </div>
-    </article>
-    `
-
+    </div>
+ `
 });
 
 
+
+
 new Vue({
-    el: '#root'
+    el: '#root',
+
+    data:{
+        showModal: false
+    }
 });
